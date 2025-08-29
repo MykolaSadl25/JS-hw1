a115326ec3f72e29183285cd025e4955fba3171a
Mykola Sadlii <mykolac08@gmail.com>
2025-08-29 17:41:27 +0100

    part 1

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const age = 14;
+console.log(age);
+const name = "Mykola Sadlii"
+console.log(name);
+const isStudent = "True";
+console.log(isStudent);
+const myString = "Sleep in peace, the mistake will not be repeated (inscription on the memorial to the victims of the bombing in Hiroshima)"
+console.log(myString);
+const myNull = "null";
+console.log(myNull);

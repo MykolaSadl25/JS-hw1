19406b2ac51de1ef9b3e4672f52e459787d02f0f
Mykola Sadlii <mykolac08@gmail.com>
2025-09-03 17:54:13 +0100

    part 2

--- js/index.js
+++ js/index.js
@@ -1,10 +1,30 @@
+// 1 task
 const age = 14;
 console.log(age);
+// 2 task
 const name = "Mykola Sadlii"
 console.log(name);
+// 3 task
 const isStudent = "True";
 console.log(isStudent);
+// 4 task
 const myString = "Sleep in peace, the mistake will not be repeated (inscription on the memorial to the victims of the bombing in Hiroshima)"
 console.log(myString);
+// 5 task
+const myNumber = 14;
+const addNumber = myNumber + 10;
+console.log(addNumber);
+// 6 task
 const myNull = "null";
 console.log(myNull);
+// 7 task
+const username = prompt("Введіть своє ім'я")
+alert(username);
+console.log(typeof (username));
+// 8 task
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+// 9 task
+const warning = alert("Увага! Дія може бути небезпечною.")
+const check = confirm("Ви впевнені, що хочете продовжити?") 
+console.log(check);
